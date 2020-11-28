a5cae1958914b29cd5d34a30a0f3d155e65c3d1d
AndreLinder <guselnikov.artur.al.@gmail.com>
2020-11-28 09:23:38 +0300

    Добавьте файлы проекта.

---
+++ MSG by AL (XAML)/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using ConnectionDB;
+
+namespace MSG_by_AL__XAML_
+{
+    public partial class MainWindow : Window
+    {
+        //Создание объекта подключения к БД
+        MySqlConnection connection = DBUtils.GetDBConnection();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Соединение установлено!", "Соединение", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ MSG by AL (XAML)/Resource/DBUtils.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ConnectionDB
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "192.168.0.130";
+            int port = 3306;
+            string database = "server_chats";
+            string username = "Andre Linder";
+            string password = "gus.ar.1628652470";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+
+    }
+}
