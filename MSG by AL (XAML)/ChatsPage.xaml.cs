f6bfe0974a430bc2e691c89014dce0b5c53fc901
Andre Linden <guselnikov.artur.al@gmail.com>
2021-01-24 17:01:09 +0300

    +Добавлены диалоговые окна отправки сообщения и списка пользователей +Проработан (не полностью) шаблон ListBoxItemTemplate, нужно сделать также с Meesage_List -Пока нет обработанных событий, связанных с БД -Исключения не обрабатывались

--- MSG by AL (XAML)/ChatsPage.xaml.cs
+++ MSG by AL (XAML)/ChatsPage.xaml.cs
@@ -89,22 +89,37 @@ namespace MSG_by_AL__XAML_
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //Поиск пользователей
+        private void User_Search_Changed(object sender, RoutedEventArgs e)
         {
             try
             {
-                TextBlock block1 = new TextBlock();
-                block1.HorizontalAlignment = HorizontalAlignment.Right;
-                block1.Background = (Brush)Application.Current.Resources["IsMouseOverColor"];
-                block1.Foreground = (Brush)Application.Current.Resources["TextColor2"];
-                block1.Text = "1234";
-                Message_List.Items.Add(block1);
-                TextBlock block2 = new TextBlock();
-                block2.HorizontalAlignment = HorizontalAlignment.Left;
-                block2.Background = (Brush)Application.Current.Resources["BorderBrush"];
-                block2.Foreground = (Brush)Application.Current.Resources["TextColor1"];
-                block2.Text = "5678";
-                Message_List.Items.Add(block2);
+                //Открываем соединение
+                connection.Open();
+
+                //Строка запроса на поиск пользователей в БД
+                string sql_cmd = "SELECT server_chats.users.ID, server_chats.users.Name FROM server_chats.users WHERE server_chats.users.Name=@NAME";
+
+                //Создаём команду для запроса в БД
+                MySqlCommand cmd = connection.CreateCommand();
+
+                //Добавляем параметры в команду
+                MySqlParameter name_parameter = new MySqlParameter("@NAME", MySqlDbType.VarChar);
+                name_parameter.Value = User_Search.Text;
+                cmd.Parameters.Add(name_parameter);
+
+                //...
+                using (DbDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+
+                        }
+                    }
+                }
+
             }
             catch (Exception ex)
             {
@@ -112,7 +127,56 @@ namespace MSG_by_AL__XAML_
             }
             finally
             {
+                //Закрываем соединение
+                connection.Close();
+            }
+        }
 
+        //Отпрвака сообщения
+        private void Send_Message_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Открываем соединение
+                connection.Open();
+
+                //Строка запроса для БД (недописана)
+                string sql_cmd = "INSERT INTO server_chats.";
+                Border baseform = new Border();
+                Border baseform2 = new Border();
+                baseform.BorderThickness = new Thickness(2);
+                baseform.BorderBrush = (Brush)Application.Current.Resources["IsMouseOverColor"];
+                baseform.CornerRadius = new CornerRadius(10);
+                baseform.Background = (Brush)Application.Current.Resources["BorderBrush"];
+                baseform.HorizontalAlignment = HorizontalAlignment.Center;
+                baseform.VerticalAlignment = VerticalAlignment.Center;
+
+                TextBlock block1 = new TextBlock();
+                block1.TextWrapping = TextWrapping.Wrap;
+                block1.Text = "Привет\nКак дела?000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+                block1.HorizontalAlignment = HorizontalAlignment.Center;
+                block1.VerticalAlignment = VerticalAlignment.Center;
+                baseform2.Child = block1;
+                Message_List.Items.Add(baseform2);
+
+                TextBlock block2 = new TextBlock();
+                block2.Text = "Привет, хорошо!\n Твои как?";
+                baseform.Child = block2;
+                Message_List.Items.Add(baseform);
+
+                Message msg = new Message();
+                msg.Message_Text = "1234";
+                User_List.Items.Clear();
+                User_List.Items.Add(msg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                //Закрываем соединение
+                connection.Close();
             }
 
         }
---
+++ MSG by AL (XAML)/Resource/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSG_by_AL__XAML_.Resource
+{
+    public class Message
+    {
+        //Пустой конструктор
+        public Message()
+        {
+        }
+
+        //Свойство, текст сообщения
+        public string Message_Text { get; set; }
+
+        //Свойсто дат отправки
+        public string Message_Date { set; get; }
+
+
+    }
+}
